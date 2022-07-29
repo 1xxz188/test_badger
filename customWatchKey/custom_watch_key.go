e4197443f84fb5004f13f2472f1c426ec23c01ea
xiangzhixu <1009159531@qq.com>
2022-07-29 17:11:16 +0800

    添加并发watch组件

---
+++ customWatchKey/custom_watch_key.go
@@ -0,0 +1,117 @@
+package customWatchKey
+
+import (
+	"errors"
+	"sync"
+)
+
+var (
+	ErrWatchVersionConflicts = errors.New("watch key conflicts")
+	ErrWatchKeyNotExist      = errors.New("watch key not exist")
+	ErrWatchKeyInitParameter = errors.New("watch key int parameter invalid")
+)
+
+type watchInfo struct {
+	lock    sync.RWMutex
+	version uint32
+}
+
+type concurrentMapShared struct {
+	items map[string]*watchInfo
+	lock  sync.RWMutex
+}
+
+type WatchKeyMgr struct {
+	shardCount     uint32
+	shardCountMask uint32
+	m              []concurrentMapShared
+}
+
+func getAlign(shardCount uint32) uint32 {
+	//2的幂次方对齐
+	temp := uint32(1) << 1
+	for i := 2; shardCount > temp; i++ {
+		temp = uint32(1) << i
+	}
+	return temp
+}
+
+func New(shardCount uint32) (*WatchKeyMgr, error) {
+	if shardCount < 1 {
+		return nil, ErrWatchKeyInitParameter
+	}
+	needShardCount := getAlign(shardCount)
+	s := &WatchKeyMgr{
+		shardCount:     needShardCount,
+		shardCountMask: needShardCount - 1,
+		m:              make([]concurrentMapShared, needShardCount),
+	}
+	for i := uint32(0); i < s.shardCount; i++ {
+		s.m[i].items = make(map[string]*watchInfo)
+	}
+	return s, nil
+}
+
+func (s *WatchKeyMgr) Read(key string, fn func(keyVersion uint32) error) (err error) {
+	shard := s.getShard(key)
+	shard.lock.RLock()
+	// Get item from shard.
+	val, ok := shard.items[key]
+
+	if ok {
+		shard.lock.RUnlock()
+		val.lock.RLock()
+		defer val.lock.RUnlock() //prevent panic
+		err = fn(val.version)
+	} else {
+		shard.lock.RUnlock()
+		shard.lock.Lock()
+		val, ok := shard.items[key]
+		if !ok {
+			val = &watchInfo{
+				version: 1,
+			}
+			shard.items[key] = val
+		}
+		shard.lock.Unlock()
+		val.lock.RLock()
+		defer val.lock.RUnlock()
+		err = fn(val.version)
+	}
+	return err
+}
+
+func (s *WatchKeyMgr) Write(key string, keyVersion uint32, fn func(keyVersion uint32) error) error {
+	shard := s.getShard(key)
+	shard.lock.RLock()
+	// Get item from shard.
+	val, ok := shard.items[key]
+	if !ok {
+		shard.lock.RUnlock()
+		return ErrWatchKeyNotExist
+	}
+	shard.lock.RUnlock()
+	val.lock.Lock()
+	defer val.lock.Unlock() //prevent panic
+	if keyVersion != val.version {
+		return ErrWatchVersionConflicts
+	}
+	err := fn(val.version)
+	val.version += 1
+	return err
+}
+
+func fnv32(key string) uint32 {
+	hash := uint32(2166136261)
+	const prime32 = uint32(16777619)
+	keyLength := len(key)
+	for i := 0; i < keyLength; i++ {
+		hash *= prime32
+		hash ^= uint32(key[i])
+	}
+	return hash
+}
+
+func (s *WatchKeyMgr) getShard(key string) *concurrentMapShared {
+	return &s.m[fnv32(key)&s.shardCountMask]
+}
---
+++ customWatchKey/custom_watch_key_test.go
@@ -0,0 +1,123 @@
+package customWatchKey
+
+import (
+	"github.com/stretchr/testify/require"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestReadWrite(t *testing.T) {
+	t.Parallel()
+	require.EqualValues(t, 2, getAlign(2))
+	require.EqualValues(t, 4, getAlign(3))
+	require.EqualValues(t, 4, getAlign(4))
+	require.EqualValues(t, 8, getAlign(5))
+	require.EqualValues(t, 16, getAlign(15))
+	require.EqualValues(t, 32, getAlign(19))
+
+	s, err := New(5)
+	require.NoError(t, err)
+
+	err = s.Write("key1", 1, func(keyVersion uint32) error {
+		require.Equal(t, uint32(1), keyVersion)
+		return nil
+	})
+	require.Equal(t, ErrWatchKeyNotExist, err)
+
+	err = s.Read("key1", func(keyVersion uint32) error {
+		require.Equal(t, uint32(1), keyVersion)
+		return nil
+	})
+	require.NoError(t, err)
+
+	err = s.Write("key1", 1, func(keyVersion uint32) error {
+		require.Equal(t, uint32(1), keyVersion)
+		return nil
+	})
+	require.NoError(t, err)
+
+	err = s.Write("key1", 2, func(keyVersion uint32) error {
+		require.Equal(t, uint32(2), keyVersion)
+		return nil
+	})
+	require.NoError(t, err)
+
+	err = s.Read("key1", func(keyVersion uint32) error {
+		require.Equal(t, uint32(3), keyVersion)
+		return nil
+	})
+	require.NoError(t, err)
+}
+
+// TestWatchGetConcurrency Test for SetIfAbsent but not ok
+func TestWatchGetConcurrency(t *testing.T) {
+	t.Parallel()
+	s, err := New(16)
+	require.NoError(t, err)
+
+	wg := sync.WaitGroup{}
+	wg.Add(100)
+	beginChan := make(chan struct{})
+	for i := 0; i < 100; i++ {
+		go func() {
+			<-beginChan
+			defer wg.Done()
+			err := s.Read("key1", func(keyVersion uint32) error {
+				require.Equal(t, uint32(1), keyVersion)
+				return nil
+			})
+			require.NoError(t, err)
+		}()
+	}
+	time.Sleep(time.Millisecond * 10)
+	close(beginChan)
+	wg.Wait()
+}
+
+// TestWatchUpdateConcurrency Test for KVWatchKey should exist when conflict
+func TestWatchUpdateConcurrency(t *testing.T) {
+	t.Parallel()
+	s, err := New(16)
+	require.NoError(t, err)
+
+	err = s.Read("key1", func(keyVersion uint32) error {
+		require.Equal(t, uint32(1), keyVersion)
+		return nil
+	})
+	require.NoError(t, err)
+
+	wg := sync.WaitGroup{}
+	wg.Add(100)
+	beginChan := make(chan struct{})
+	conflictCnt := int32(0)
+	for i := uint32(1); i <= 100; i++ {
+		go func(version uint32) {
+			<-beginChan
+			defer wg.Done()
+			for {
+				err := s.Write("key1", version, func(keyVersion uint32) error {
+					require.Equal(t, version, keyVersion)
+					return nil
+				})
+				if err != nil {
+					require.Equal(t, ErrWatchVersionConflicts, err)
+					atomic.AddInt32(&conflictCnt, 1)
+				} else {
+					break
+				}
+			}
+		}(i)
+	}
+	time.Sleep(time.Millisecond * 10)
+	close(beginChan)
+	wg.Wait()
+
+	t.Log("conflictCnt: ", conflictCnt)
+	err = s.Read("key1", func(keyVersion uint32) error {
+		require.Equal(t, int32(101), int32(keyVersion))
+		return nil
+	})
+	require.NoError(t, err)
+}
